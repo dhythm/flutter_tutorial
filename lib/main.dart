f339904da6059f7658229ef8175b317c47544e5e
dhythm <yuta.okada.20@gmail.com>
2021-02-28 10:49:39 +0900

    feat(tutorial): completed Basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,70 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+
+// void main() => runApp(Center(
+//         child: Text(
+//       'Hellow, world!',
+//       textDirection: TextDirection.ltr,
+//     )));
+
+void main() {
+  runApp(MaterialApp(
+      title: 'My app',
+      home: SafeArea(
+        child: MyScafforld(),
+      )));
+}
+
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigation menu',
+              onPressed: null),
+          Expanded(child: title),
+          IconButton(
+              icon: Icon(Icons.search), tooltip: 'Search', onPressed: null),
+        ],
+      ),
+    );
+  }
+}
+
+class MyScafforld extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text('Hello, world!'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
