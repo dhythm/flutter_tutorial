48ba1beb3707a741ca75d6fede7f85f78aaee97a
dhythm <yuta.okada.20@gmail.com>
2021-02-28 11:17:17 +0900

    feat(tutorial): completed Changing widgets in response to input

--- lib/main.dart
+++ lib/main.dart
@@ -104,3 +104,48 @@ class MyButton extends StatelessWidget {
     );
   }
 }
+
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+  final VoidCallback onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPressed, child: Text('Increment'));
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        ElevatedButton(onPressed: _increment, child: Text('Increment')),
+        Text('Counte: $_counter')
+      ],
+    );
+  }
+}
