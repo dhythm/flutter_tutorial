2b6a771a7d6034920c29919a9ba8e89b7e6437dd
dhythm <yuta.okada.20@gmail.com>
2021-03-02 21:32:09 +0900

    feat(tutorial): The parent widget manages the widget’s state

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,64 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(active: _active, onChanged: _handleTapboxChanged),
+    );
+  }
+}
 
 class TapboxA extends StatefulWidget {
   TapboxA({Key key}) : super(key: key);
 
   @override
   _TapboxAState createState() => _TapboxAState();
+}
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.grey[600]),
+      ),
+    );
+    ;
+  }
 }
 
 class _TapboxAState extends State<TapboxA> {
@@ -49,7 +101,8 @@ class MyApp extends StatelessWidget {
             title: Text('Flutter Demo'),
           ),
           body: Center(
-            child: TapboxA(),
+            // child: TapboxA(),
+            child: TapboxB(),
           ),
         ));
   }
