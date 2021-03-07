48c492556f7e5a9d181f54d4a0c363f12c8746a9
dhythm <yuta.okada.20@gmail.com>
2021-03-07 11:36:28 +0900

    feat(tutorial): Navigator 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,61 @@
 import 'package:flutter/material.dart';
 
-import './my_home_page.dart';
-
 void main() {
-  runApp(MyApp());
+  runApp(Nav2App());
 }
 
-class MyApp extends StatelessWidget {
+class Nav2App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: MyHomePage(title: 'FlutterDemo Home Page'));
+      home: HomeScreen(),
+    );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: TextButton(
+            child: Text('View Details'),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen();
+              }));
+            },
+          ),
+        ));
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: TextButton(
+            child: Text('Pop!'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ));
+  }
+}
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: 'Flutter Demo',
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//           visualDensity: VisualDensity.adaptivePlatformDensity,
+//         ),
+//         home: MyHomePage(title: 'FlutterDemo Home Page'));
+//   }
+// }
