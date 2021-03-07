dbd1d082227a8d39c20d998d044041793092fb65
dhythm <yuta.okada.20@gmail.com>
2021-03-07 16:21:44 +0900

    feat(tutorial): Animations - 2

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,28 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     super.initState();
     controller =
         AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
-      ..addListener(() {
-        setState(() {});
-      });
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
     controller.forward();
   }
 
   @override
+  Widget build(BuildContext context) => AnimatedLogo(
+        animation: animation,
+      );
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
   Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
     return Center(
       child: Container(
         margin: EdgeInsets.symmetric(vertical: 10),
@@ -34,11 +47,5 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
         child: FlutterLogo(),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
   }
 }
