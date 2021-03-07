1cf7b557fcb2895dac20f021ecff38f34f85c2dd
dhythm <yuta.okada.20@gmail.com>
2021-03-07 14:47:29 +0900

    introduction of Navigator 2.0 -RouterDelegate-

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/navigator_1.dart';
 
 void main() {
   runApp(BooksApp());
@@ -131,5 +132,107 @@ class BookDetailsScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class BookRoutePath {
+  final int id;
+  final bool isUnknown;
+
+  BookRoutePath.home()
+      : id = null,
+        isUnknown = false;
+
+  BookRoutePath.details(this.id) : isUnknown = false;
+
+  BookRoutePath.unknown()
+      : id = null,
+        isUnknown = true;
+
+  bool get isHomePage => id == null;
+  bool get isDetailsPage => id != null;
+}
+
+class BookRouteDelegate extends RouterDelegate<BookRoutePath>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<BookRoutePath> {
+  final GlobalKey<NavigatorState> navigatroKey;
+
+  Book _selectedBook;
+  bool show404 = false;
+
+  List<Book> books = [
+    Book('Stranger in a Strange Land', 'Rovert A. Heinlein'),
+    Book('Foundation', 'Isaac Asimov'),
+    Book('Fahrenheit 451', 'Ray Bradbury'),
+  ];
+
+  BookRouteDelegate() : navigatroKey = GlobalKey<NavigatorState>();
+
+  BookRoutePath get currentConfiguration {
+    if (show404) {
+      return BookRoutePath.unknown();
+    }
+
+    return _selectedBook == null
+        ? BookRoutePath.home()
+        : BookRoutePath.details(books.indexOf(_selectedBook));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+        key: navigatorKey,
+        pages: [
+          MaterialPage(
+              child: BooksListScreen(
+                books: books,
+                onTapped: _handleBookTapped,
+              ),
+              key: ValueKey('BooksListPage')),
+          if (show404)
+            MaterialPage(child: UnknownScreen(), key: ValueKey('UnknownPage'))
+          else if (_selectedBook != null)
+            BookDetailsPage(book: _selectedBook)
+        ],
+        onPopPage: (route, result) {
+          if (!route.didPop(result)) {
+            return false;
+          }
+          _selectedBook = null;
+          show404 = false;
+          notifyListeners();
+
+          return true;
+        });
+  }
+
+  void _handleBookTapped(Book book) {
+    _selectedBook = book;
+    notifyListeners();
+  }
+
+  @override
+  GlobalKey<NavigatorState> get navigatorKey => throw UnimplementedError();
+
+  @override
+  Future<void> setNewRoutePath(BookRoutePath path) async {
+    if (path.isUnknown) {
+      _selectedBook = null;
+      show404 = true;
+      return;
+    }
+
+    if (path.isDetailsPage) {
+      if (path.id < 0 || path.id > books.length - 1) {
+        show404 = true;
+        return;
+      }
+
+      _selectedBook = books[path.id];
+    } else {
+      _selectedBook = null;
+    }
+
+    show404 = false;
   }
 }
